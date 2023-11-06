afe6b0eb9d8d8c89d2b41ccbb541e2ffc4e21680
Cuong0939 <88544653+Cuong0939@users.noreply.github.com>
2023-11-06 07:12:10 +0700

    finish program

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello world")
+const collectiveWisdom = {
+    signInfo: ['star', 'moon', 'sun', 'comet'],
+    fortuneOutput: ['terrible luck', 'bad luck', 'ok luck', 'good luck', 'great luck'],
+    advice: ['go out and eat', 'not read this', 'play more', 'trust no one']
+  }
+
+function getRandomChoice(num){
+    return Math.floor(Math.random() * (num.length))
+}
+
+let Advices = [];
+
+for (arr in collectiveWisdom){
+    console.log(collectiveWisdom[arr][getRandomChoice(collectiveWisdom[arr])])
+}
